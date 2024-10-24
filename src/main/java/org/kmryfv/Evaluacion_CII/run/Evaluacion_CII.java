6113cf891316fdfad78b51bae581c25c4f033e56
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2024-10-24 17:14:55 -0600

    Initial commit

---
+++ src/main/java/org/kmryfv/Evaluacion_CII/run/Evaluacion_CII.java
@@ -0,0 +1,18 @@
+package org.kmryfv.Evaluacion_CII.run;
+
+import org.openxava.util.*;
+
+/**
+ * Execute this class to start the application.
+ *
+ * With OpenXava Studio/Eclipse: Right mouse button > Run As > Java Application
+ */
+
+public class Evaluacion_CII {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("Evaluacion_CII-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml
+		AppServer.run("Evaluacion_CII"); // Use AppServer.run("") to run in root context
+	}
+
+}
