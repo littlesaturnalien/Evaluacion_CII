23701fbf0d5749b1e08ae8afe9fadb81de00d49e
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2024-10-24 17:57:17 -0600

    Se completaron las ultimas clases

--- src/main/java/org/kmryfv/Evaluacion_CII/model/Advance.java
+++ src/main/java/org/kmryfv/Evaluacion_CII/model/Advance.java
@@ -2,12 +2,25 @@ package org.kmryfv.Evaluacion_CII.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.openxava.annotations.Stereotype;
 
-import javax.persistence.Embeddable;
+import javax.persistence.*;
+import java.util.Date;
 
 @Embeddable
 @Getter
 @Setter
 public class Advance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @ManyToOne
+    private Task task;
+
+    @Stereotype("DATE")
+    private Date fecha;
+
+    @Stereotype("MEMO")
+    private String comentario;
 }
